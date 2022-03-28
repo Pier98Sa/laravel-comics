44d48ff03635c384cb962e1e3f8a6fad3d51d193
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-28 19:04:28 +0200

    Esercizio completato

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,27 +1,41 @@
 <header>
+  <div class="header-top">
+    <div class="container">
+      <span>DC Power&trade; Visa&reg; </span>
+      <span>Additional Dc Sites &#9660;</span>
+    </div>
+    
+  </div>
+  <div class="header-bottom">
     <nav class="container">
-        <!--nav sx-->
-        <div class="nav-left">
-          <img src="{{asset('img/dc-logo.png')}}" alt="">
-        </div>
-    
-        <!--nav dx-->
-        <div class="nav-right">
-          <ul>
-            <li><a href="http://">Characters</a></li>
-            <li ><a href="http://" class="active">Comics</a></li>
-            <li><a href="http://">Movies</a></li>
-            <li><a href="http://">Tv</a></li>
-            <li><a href="http://">Games</a></li>
-            <li><a href="http://">Collectibles</a></li>
-            <li><a href="http://">Videos</a></li>
-            <li><a href="http://">Fans</a></li>
-            <li><a href="http://">News</a></li>
-            <li><a href="http://">Shop</a></li>
-          </ul>
+      <!--nav sx-->
+      <div class="nav-left">
+        <img src="{{asset('img/dc-logo.png')}}" alt="">
+      </div>
+  
+      <!--nav dx-->
+      <div class="nav-right">
+        <ul>
+          <li><a href="http://">Characters</a></li>
+          <li ><a href="http://" class="active">Comics</a></li>
+          <li><a href="http://">Movies</a></li>
+          <li><a href="http://">Tv</a></li>
+          <li><a href="http://">Games</a></li>
+          <li><a href="http://">Collectibles</a></li>
+          <li><a href="http://">Videos</a></li>
+          <li><a href="http://">Fans</a></li>
+          <li><a href="http://">News</a></li>
+          <li><a href="http://">Shop</a></li>
+        </ul>
 
+        <div class="input-box">
           <input type="search" name="search" placeholder="Search">
+          <i class="fa-solid fa-magnifying-glass"></i>
         </div>
-    
-      </nav>
+        
+      </div>
+  
+    </nav>
+  </div>
+  
 </header>
